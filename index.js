c41ac3f59286434ec569edd463a11195305b2eea
Anthony <anthonykimlao@gmail.com>
2024-01-06 06:15:35 -0600

    complete lab

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const dodger = document.getElementById("dodger");
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
@@ -21,7 +21,7 @@ function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left < 360) {
+    if (left <360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
