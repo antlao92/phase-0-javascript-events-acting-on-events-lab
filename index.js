257d4e54d3ab0819a24f479ba7823aa31f57d8c4
Anthony <anthonykimlao@gmail.com>
2024-01-06 06:19:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -14,20 +14,17 @@ function moveDodgerLeft() {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
+    else if(e.key === "ArrowRight"){
+      moveDodgerRight();
+    }
   });
 
 
   function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   
     if (left <360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
